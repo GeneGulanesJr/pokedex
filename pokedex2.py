af42171f0bce2932d899a429bfd269cd3a0964d0
GeneGulanesJr <gulanesgene@gmail.com>
2023-10-03 22:24:42 +0800

    Coding3

--- pokedex2.py
+++ pokedex2.py
@@ -1,15 +1,29 @@
 import requests
 import sys
 
-pokemon_list = []
-
 def search_pokemon(name):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
-    #response = requests.get("https://pokeapi.co/api/v2/pokemon/charmander")
     pokemon = response.json()
-    print("Name: "+ pokemon["name"])
-    print("ID: " + str(pokemon["id"]))
-    print("Base XP: " + str(pokemon["base_experience"]))
+
+    hp = pokemon["stats"][0]["base_stat"]
+    hp_str = f"{hp}/{hp}"
+    moves = ", ".join(move_data["move"]["name"] for move_data in pokemon["moves"])
+    held_items = ", ".join(item["item"]["name"] for item in pokemon["held_items"])
+    
+    print("Name:", pokemon["name"])
+    print("HP:", hp_str)
+    print("Attacks:", moves)
+    print("Held Items:", held_items)
+
+def main():
+    if len(sys.argv) != 7:
+        print("Usage: python script.py <pokemon_name1> <pokemon_name2> <pokemon_name3> <pokemon_name4> <pokemon_name5> <pokemon_name6>")
+    else:
+        pokemon_names = sys.argv[1:]
+        for name in pokemon_names:
+            search_pokemon(name)
 
 if __name__ == "__main__":
-    search_pokemon(sys.argv[1])
+    main()
+
+# sample line python3 pokedex2.py charmander bulbasaur pikachu squirtle jigglypuff meowth
