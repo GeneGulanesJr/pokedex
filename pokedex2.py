c0fda1ae48d54f959f403400f2515660637f02ec
GeneGulanesJr <gulanesgene@gmail.com>
2023-10-03 22:04:11 +0800

    coding challenge1

---
+++ pokedex2.py
@@ -0,0 +1,15 @@
+import requests
+import sys
+
+pokemon_list = []
+
+def search_pokemon(name):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+    #response = requests.get("https://pokeapi.co/api/v2/pokemon/charmander")
+    pokemon = response.json()
+    print("Name: "+ pokemon["name"])
+    print("ID: " + str(pokemon["id"]))
+    print("Base XP: " + str(pokemon["base_experience"]))
+
+if __name__ == "__main__":
+    search_pokemon(sys.argv[1])
